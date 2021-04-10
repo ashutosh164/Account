58fce7e84a2dd37d9ee973514657956eb9f49cd1
Ashutosh pradhan <ashutoshpradhan164@gmail.com>
2021-04-10 12:53:43 +0530

    accoun

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import datetime
+import pytz
+
+
 class Account:
+
     def __init__(self,name,balance):
         self.name = name
         self.balance = balance
+        self.transaction_list = []
         print('Account created for ' + self.name)
 
     def deposit(self,amount):
         if amount > 0:
             self.balance += amount
             self.show_balance()
+            self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()),amount))
 
     def withdraw(self,amount):
         if 0 < amount <= self.balance:
@@ -19,18 +26,26 @@ class Account:
     def show_balance(self):
         print(f"balance is {self.balance}")
 
+    def show_transaction(self):
+        for date, amount in self.transaction_list:
+            if amount >= 0:
+                tran_type = 'deposited'
+            else:
+                tran_type = 'withdrawn'
+                amount *= -1
+            print(f"{amount} {tran_type} on {date} (local time was {date.astimezone()})")
+            # print("{} {} on {} (local time was {})".format(amount,tran_type,date,date.astimeone()))
+
 
 if __name__ == '__main__':
     ashu = Account('Ashu',0)
     ashu.show_balance()
 
     ashu.deposit(1000)
-    # ashu.show_balance()
-    ashu.withdraw(500)
-    # ashu.show_balance()
-    ashu.withdraw(2000)
-    ashu.deposit(5555555555555555)
-    
+    ashu.withdraw(100)
+    ashu.deposit(2)
+    ashu.show_transaction()
+
 
 
 
